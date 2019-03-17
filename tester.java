3df545f232fd9327e1e8dad46756092d10f91e68
cohyunoh <coh00@stuy.edu>
2019-03-17 12:18:14 -0400

    for some reason it is not working with the tester online

--- tester.java
+++ tester.java
@@ -1,37 +1,40 @@
 import java.util.Arrays;
+import java.util.Random;
 public class tester{
   public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
+    System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
+    int[]MAX_LIST = {100000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long mtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Merge.mergesort(data2);
+          t2 = System.currentTimeMillis();
+          mtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println(Arrays.toString(data2));
+            System.out.println(Arrays.toString(data1));
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
         }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Merge.mergesort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*mtime/btime);
       }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      System.out.println();
     }
-    System.out.println();
   }
-}
 }
