30369c5dbb2981b46f4bb92f8f747c7e7505d2d1
cohyunoh <coh00@stuy.edu>
2019-03-19 11:47:13 -0400

    getting stackoverflow errors

--- Merge.java
+++ Merge.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    int k = data.length / 2;
+    int k = 2;
     mergesort(data, 0, data.length - 1, k);
   }
 
